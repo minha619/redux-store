1e2964c3f76a0bdeea74b5394d465aadfc78f15f
minha619 <minha619@gmail.com>
2022-01-30 14:51:09 -0500

    refratored code to use redux

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from "../../utils/GlobalState";
+// import { useStoreContext } from "../../utils/GlobalState";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
+import { useDispatch, useSelector } from 'react-redux'
+
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
+  // const [state, dispatch] = useStoreContext();
 
   const {
     image,
@@ -15,6 +17,12 @@ function ProductItem(item) {
     price,
     quantity
   } = item;
+
+  const state = useSelector((state) => {
+    return state;
+  })
+
+  const dispatch = useDispatch();
 
   const { cart } = state
 
