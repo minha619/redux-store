76ffb9be5d8153ed70209f26c0b0c939fc799684
minha619 <minha619@gmail.com>
2022-01-30 20:13:02 -0500

    update

---
+++ client/build/precache-manifest.0e3632500ecda58ca7656ff04c883275.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c31bc62d8931651dcc3d0a0aaee1ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0db6b12735bc4241a479",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "eadf49bfb10640b8accf",
+    "url": "/static/js/2.2950f898.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.2950f898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0db6b12735bc4241a479",
+    "url": "/static/js/main.d71a95f3.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
